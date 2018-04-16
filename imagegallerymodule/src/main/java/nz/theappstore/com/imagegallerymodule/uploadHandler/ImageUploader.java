e525c2e90dcce9ffa20e55454ba4d176478008a2
vyomkesh <vyomkeshjha@yahoo.com>
2018-04-17 03:47:19 +0530

    Changed to native, broken upload!

--- imagegallerymodule/src/main/java/nz/theappstore/com/imagegallerymodule/uploadHandler/ImageUploader.java
+++ imagegallerymodule/src/main/java/nz/theappstore/com/imagegallerymodule/uploadHandler/ImageUploader.java
@@ -3,6 +3,7 @@ package nz.theappstore.com.imagegallerymodule.uploadHandler;
 import android.content.Context;
 import android.util.Log;
 
+import net.gotev.uploadservice.Logger;
 import net.gotev.uploadservice.MultipartUploadRequest;
 import net.gotev.uploadservice.UploadNotificationConfig;
 
@@ -21,10 +22,12 @@ public class ImageUploader {
         try {
             File file = new File(filePath);
             Log.i("FILE_UPLOAD", "exists = "+file.exists()+ " size= "+file.length());
+            Logger.setLogLevel(Logger.LogLevel.DEBUG);
+
             return new MultipartUploadRequest(context, uploadUrl) {}
                             // starting from 3.1+, you can also use content:// URI string instead of absolute file
                             .addFileToUpload(filePath, "file")
-                            .setNotificationConfig(new UploadNotificationConfig())
+                            .setNotificationConfig(new UploadNotificationConfig() )
                             .setMaxRetries(2)
                             .setBasicAuth("Ram","Crimson")
                             .startUpload();
